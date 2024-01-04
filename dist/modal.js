acec4f0f5292702cba5146deedb6573cfd07369e
Benjamin Corroyez <benjamincorroyez@hotmail.fr>
2024-01-04 18:09:32 +0100

    mise a jour du fichier dist

--- dist/modal.js
+++ dist/modal.js
@@ -10,18 +10,26 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 const Modal = _ref => {
   let {
     show,
-    children,
-    onClose
+    onClose,
+    children
   } = _ref;
   if (!show) {
     return null;
   }
+  const handleBackdropClick = e => {
+    if (e.target.classList.contains('modal-backdrop')) {
+      onClose();
+    }
+  };
   return /*#__PURE__*/_react.default.createElement("div", {
-    className: "modal-backdrop"
+    className: "modal-backdrop",
+    onClick: handleBackdropClick
   }, /*#__PURE__*/_react.default.createElement("div", {
-    className: "modal"
-  }, children, /*#__PURE__*/_react.default.createElement("button", {
+    className: "modal",
+    onClick: e => e.stopPropagation()
+  }, /*#__PURE__*/_react.default.createElement("button", {
+    className: "close-button",
     onClick: onClose
-  }, "Fermer")));
+  }, "\xD7"), children));
 };
 var _default = exports.default = Modal;
